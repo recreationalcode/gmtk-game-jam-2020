b0ef8cc24964c16dddacf5c873170e05782ef44a
Neel Shah <ShahN@valassis.com>
2020-07-10 21:57:36 -0500

    Adding basic zombie player and auto human controllers

---
+++ Assets/Scripts/HumanController.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+using System.Collections;
+
+public class HumanController : MonoBehaviour
+{
+  public float moveSpeed = 1f;
+  public bool shouldMove = true;
+  public Rigidbody2D rigidBody;
+  public Vector3 target;
+
+  private bool isMoving = false;
+
+  private Vector3 currentTarget;
+  private Vector3 futureTarget;
+
+  void Start()
+  {
+    currentTarget = target;
+    futureTarget = transform.position;
+  }
+
+  void Update()
+  {
+    if (!isMoving)
+    {
+      isMoving = true;
+      StartCoroutine(MoveToTarget());
+    }
+  }
+
+  IEnumerator MoveToTarget()
+  {
+    float t = 0.0f;
+    float timeToTarget = Vector3.Distance(transform.position, currentTarget) / moveSpeed;
+
+    while (t < timeToTarget)
+    {
+      t += Time.deltaTime;
+      rigidBody.MovePosition(Vector3.Lerp(futureTarget, currentTarget, t / timeToTarget));
+      yield return new WaitForFixedUpdate();
+    }
+
+    Vector3 target = currentTarget;
+    currentTarget = futureTarget;
+    futureTarget = target;
+
+    isMoving = false;
+  }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+  public float moveSpeed = 5f;
+
+  public Rigidbody2D rigidBody;
+
+  Vector2 movement;
+
+  void Update()
+  {
+    movement.x = Input.GetAxisRaw("Horizontal");
+    movement.y = Input.GetAxisRaw("Vertical");
+  }
+
+  void FixedUpdate()
+  {
+    rigidBody.MovePosition(rigidBody.position + (movement * moveSpeed * Time.fixedDeltaTime));
+  }
+}
